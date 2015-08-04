1669e21c042e616d632dc5c108dfcd81e52de547
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-04 21:12:04 +0900

    敵の配置・名前付けを関数化 / 不具合修正
    ■敵の配置・名前付けをする関数 “Placement_Naming”を追加
    　下記の5つの引数を用いる
    	 * 第一引数：コピーする敵
    	 * 第二引数：番地内の敵の配置_X（中心：０,０）
    	 * 第三引数：番地内の敵の配置_Y（中心：０,０）
    	 * 第四引数：マップのX番地
    	 * 第五引数：マップのY番地
    
    ■不具合を修正
    　・同じマップに敵を二体以上作れない不具合を修正

--- Scripts/Enemy/enemyPlacement.cs
+++ Scripts/Enemy/enemyPlacement.cs
@@ -2,7 +2,9 @@
  * 敵の配置を行う
  * 現在と１つ前のマップの敵配置を管理
  * ２つ前のマップの敵は全て削除する
+ * アタッチする敵の名前は原則”Enemy”とする
  * "mapControll.cs" と連携。
+ * 
  * 2015/07/31 Fri - Guttyon
 */
 
@@ -60,26 +62,28 @@ public class enemyPlacement : MonoBehaviour {
 
 		// 敵キャラ複製の準備をする
 		GameObject originalEnemy_01 = GameObject.Find("Enemy");
-		GameObject copyEnemy = Object.Instantiate (originalEnemy_01) as GameObject;;
+		//GameObject copyEnemy = Object.Instantiate (originalEnemy_01) as GameObject;
 
 		// マップ番地に合わせて敵キャラを配置していく
 		switch (mapNum_X) {
 		case -1:
 			switch(mapNum_Y){
 			case 0:
-				// 配コピーした敵を配置する
-				copyEnemy.transform.TransformPoint(cameraStartPosition.x -10, cameraStartPosition.y, 0);
-				
-				// 敵キャラの名前を（Enemy_X番地_Y番地）に設定する
-				copyEnemy.name = "Enemy" + "_" + mapNum_X + "_" + mapNum_Y;
+
+				/*
+				 * 敵の配置と名前付け
+				 * 第一引数：コピーする敵
+				 * 第二引数：番地内の敵の配置_X（中心：０,０）
+				 * 第三引数：番地内の敵の配置_Y（中心：０,０）
+				 * 第四引数：マップのX番地
+	 			 * 第五引数：マップのY番地
+	 			 */
+				Placement_Naming( originalEnemy_01,  -10, 0, mapNum_X, mapNum_Y);
+				Placement_Naming( originalEnemy_01,  -10, 10, mapNum_X, mapNum_Y);
 				break;
 				
 			case 1:
-				// 配コピーした敵を配置する
-				copyEnemy.transform.TransformPoint(cameraStartPosition.x -10, cameraStartPosition.y, 0);
-				
-				// 敵キャラの名前を（Enemy_X番地_Y番地）に設定する
-				copyEnemy.name = "Enemy" + "_" + mapNum_X + "_" + mapNum_Y;
+				Placement_Naming( originalEnemy_01,  -10, 0, mapNum_X, mapNum_Y);
 				break;
 			}
 
@@ -88,19 +92,11 @@ public class enemyPlacement : MonoBehaviour {
 
 			switch(mapNum_Y){
 			case 0:
-				// 配コピーした敵を配置する
-				copyEnemy.transform.TransformPoint(cameraStartPosition.x -10, cameraStartPosition.y, 0);
-				
-				// 敵キャラの名前を（Enemy_X番地_Y番地）に設定する
-				copyEnemy.name = "Enemy" + "_" + mapNum_X + "_" + mapNum_Y;
+				Placement_Naming( originalEnemy_01,  -10, 0, mapNum_X, mapNum_Y);
 				break;
 
 			case 1:
-				// 配コピーした敵を配置する
-				copyEnemy.transform.TransformPoint(cameraStartPosition.x -10, cameraStartPosition.y, 0);
-
-				// 敵キャラの名前を（Enemy_X番地_Y番地）に設定する
-				copyEnemy.name = "Enemy" + "_" + mapNum_X + "_" + mapNum_Y;
+				Placement_Naming( originalEnemy_01,  -10, 0, mapNum_X, mapNum_Y);
 				break;
 			}
 
@@ -109,24 +105,36 @@ public class enemyPlacement : MonoBehaviour {
 		case 1:
 			switch(mapNum_Y){
 			case 0:
-				// 敵を配置
-				// 配コピーした敵を配置する
-				copyEnemy.transform.TransformPoint(cameraStartPosition.x -10, cameraStartPosition.y, 0);
-				
-				// 敵キャラの名前を（Enemy_X番地_Y番地）に設定する
-				copyEnemy.name = "Enemy" + "_" + mapNum_X + "_" + mapNum_Y;
+				Placement_Naming( originalEnemy_01,  -10, 0, mapNum_X, mapNum_Y);
 				break;
 				
 			case 1:
-				// 配コピーした敵を配置する
-				copyEnemy.transform.TransformPoint(cameraStartPosition.x -10, cameraStartPosition.y, 0);
-				
-				// 敵キャラの名前を（Enemy_X番地_Y番地）に設定する
-				copyEnemy.name = "Enemy" + "_" + mapNum_X + "_" + mapNum_Y;
+				Placement_Naming( originalEnemy_01,  -10, 0, mapNum_X, mapNum_Y);
 				break;
 			}
 			break;
 		}
+	}
+
+	/*
+	 * 指定した敵をコピーして配置し、番地に応じた名前を付ける 
+	 * 
+	 * 第一引数：コピーする敵
+	 * 第二引数：番地内の敵の配置_X（中心：０,０）
+	 * 第三引数：番地内の敵の配置_Y（中心：０,０）
+	 * 第四引数：マップのX番地
+	 * 第五引数：マップのY番地
+	*/
+	public static void Placement_Naming(GameObject originalEnemy, int placement_X = 0, int placement_Y = 0, int mapNum_X = 0, int mapNum_Y = 0){
+
+		originalEnemy = GameObject.Find("Enemy");
+		GameObject copyEnemy = Object.Instantiate (originalEnemy) as GameObject;
+
+		// 配コピーした敵を配置する
+		copyEnemy.transform.position = new Vector3(cameraStartPosition.x + placement_X, cameraStartPosition.y + placement_Y, 0);
+
+		// 敵キャラの名前を（Enemy_X番地_Y番地）に設定する
+		copyEnemy.name = "Enemy" + "_" + mapNum_X + "_" + mapNum_Y;
 	}
 }
 
