00f808880675f5175fcbac81a2e642d8389f2563
Guttyon3 <y.yamaguchi0329@gmail.com>
2015-08-04 20:28:39 +0900

    SNS投稿機能プラグインと投稿ボタンの追加
    各スマホに応じて、SNSアプリ一覧を表示してメッセージや（将来的にゲームのホームページ）画像を投稿できる。
    
    SNS投稿機能プラグイン：SocialConnector.cs
    SNS投稿ボタン：SocialButton.cs

---
+++ Scripts/Plugins/SNSButton/SocialButton.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * OSに応じて共有可能なアプリ（SNS)一覧を表示
+ * 2015/08/04 Tue - Guttyon
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class SocialButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	string imagePath
+	{
+		get
+		{
+			return Application.persistentDataPath + "/image.png";
+		}
+	}
+	
+	void OnGUI () {
+		
+		// Twitter Share
+		if (GUI.Button (new Rect (215, 170, 200, 50), "SNS Share")) {
+
+			SocialConnector.Share("Message", "http://gameSite.com", imagePath);
+		}
+
+	}
+}
